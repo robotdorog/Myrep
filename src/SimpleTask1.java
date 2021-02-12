dafbc7e075642a0ca68236b5f6307db78a3d8956
robotdorog <robotdorog@gmail.com>
2021-02-12 17:44:25 +0800

    SimpleTask1 Добавил доп условие на проверку ввода пустой строки

--- src/SimpleTask1.java
+++ src/SimpleTask1.java
@@ -1,18 +1,21 @@
 public class SimpleTask1 {
 
-    public static void main( final String args[]) {
-        String str = "Hello world";
+    public static void main(final String args[]) {
+        String str = "";
         System.out.println(invStr(str));
     }
 
-    public static  String invStr(String str) {
+    public static String invStr(String str) {
         final int length = str.length(); // Длина строки
 
         if (length > 5) {
             return str.substring(0, 3) + str.substring(length - 3, length);
 
+        } else if (length > 1) {
+            return str.substring(0, 1).repeat(length);
         }
-
-            return str.substring(0,1).repeat(length);
+      else {
+          throw new IllegalArgumentException("String can't be empty");
+        }
     }
 }
