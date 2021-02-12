7bacb2815ab5945b77bc5bb09947efc2b648bf57
robotdorog <robotdorog@gmail.com>
2021-02-12 17:32:50 +0800

    SimpleTask1 убрал лишние переменные

--- src/SimpleTask1.java
+++ src/SimpleTask1.java
@@ -7,17 +7,12 @@ public class SimpleTask1 {
 
     public static  String invStr(String str) {
         final int length = str.length(); // Длина строки
-        final String firstResult; // Результат при условии длины строки больше 5
-        final String firstChar;
-        final String secondResult; // Результат при условии длины строки меньше или равно 5
+
         if (length > 5) {
-            firstResult = str.substring(0, 3) + str.substring(length - 3, length);
-            return firstResult;
+            return str.substring(0, 3) + str.substring(length - 3, length);
+
         }
-            firstChar = str.substring(0,1);
-            secondResult = firstChar.repeat(length);
 
-
-        return secondResult;
+            return str.substring(0,1).repeat(length);
     }
 }
