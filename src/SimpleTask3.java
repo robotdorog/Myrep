e83b260f5c7c88f4f6ae877062a9361557193fbb
robotdorog <robotdorog@gmail.com>
2021-04-12 16:46:31 +0800

    Задача 4

--- src/SimpleTask3.java
+++ src/SimpleTask3.java
@@ -6,7 +6,7 @@ public class SimpleTask3 {
 
     public static void main(String[] args) {
         System.out.println(zeroOne(10));
-        System.out.println(even(10));
+        System.out.println(even(20));
         System.out.println(zeroOneRandom(10));
     }
 
@@ -15,29 +15,30 @@ public class SimpleTask3 {
         for (int i = 0; i < arraySize; i++) {
             list.add(0);
             list.add(1);
-
         }
         return list;
     }
 
     public static List<Integer> even(int arraySize) {
         List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < arraySize; i++) {
-            if (i % 2 != 0)
-                list.add(i);
+        for (int i = 1; list.size() < arraySize; i = i + 2) {
+            list.add(i);
         }
         return list;
     }
 
     public static List<Integer> zeroOneRandom(int arraySize) {
         List<Integer> list = new ArrayList<>();
-        Random random = new Random();
         for (int i = 0; i < arraySize; i++) {
             list.add(1);
-            list.add(random.nextInt(2));
         }
-
+        for (int i = 0; i < arraySize / 2 - 1; i++) {
+            int randomInd = ((int) (arraySize * Math.random()));
+            list.set(randomInd, 0);
+        }
         return list;
     }
 }
+
+
+
--- src/SimpleTask4.java
+++ src/SimpleTask4.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class SimpleTask4 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SimpleTask4 {
+    public static void main(String[] args) throws IOException {
+        checkGame(5);
+
+    }
+
+    public static void checkGame(int guessNumb) throws IOException {
+        //int item = 6;
+        int item = (int) (Math.random() * 11);
+
+        System.out.println("Вам необходимо угадать число за " + guessNumb + " попыток");
+        for (int guessCount = 0; guessCount <= guessNumb; guessCount++) {
+
+            if (guessCount == guessNumb) {
+                System.out.println("");
+                System.out.println("Попытки закончились");
+                break;
+            } else {
+                System.out.println("Введите число");
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                String strPrompt = reader.readLine();
+                int prompt = Integer.parseInt(strPrompt);
+
+                if (prompt == item) {
+                    System.out.println("Вы угадали! Число попыток " + (guessCount + 1));
+                    break;
+                } else if (prompt < item) {
+                    System.out.println("Вы не угадали! Загаданное число больше");
+                    System.out.println("Осталось попыток: " + (guessNumb - guessCount - 1));
+
+                } else {
+                    System.out.println("Вы не угадали! Загаданное число меньше");
+                }
+
+            }
+
+        }
+    }
 }
