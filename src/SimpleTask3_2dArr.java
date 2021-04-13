0e9daa84e5caceb7f57525d75eb2bc9266997ca4
robotdorog <robotdorog@gmail.com>
2021-04-13 23:21:25 +0800

    Добавлена задача с двумерным массивом

--- src/SimpleTask3_2dArr.java
+++ src/SimpleTask3_2dArr.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class SimpleTask3_2dArr {
+import java.util.Arrays;
+import java.util.Random;
+
+public class SimpleTask3_2dArr {
+    public static void main(String[] args) {
+        System.out.println("Сумма чисел двумерного массива равна: " + array2D(3));
+    }
+
+    public static int array2D(int n) {
+        int[][] myArray = new int[n][n];
+        // Random random = new Random();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                myArray[i][j] = (int) (10 * Math.random());
+                sum += myArray[i][j];
+                System.out.print(myArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+        return sum;
+    }
 }
