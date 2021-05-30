650246129a3fb6e860eaba1607b9c51d16719f51
robotdorog <robotdorog@gmail.com>
2021-05-30 13:35:24 +0800

    Реализован алгоритм нахождения медианы в чисел

--- src/Mediana.java
+++ src/Mediana.java
@@ -2,43 +2,29 @@ import java.util.ArrayList;
 
 public class Mediana {
 
-    public ArrayList<Integer> difference(ArrayList<Integer> diffList) {
+    private ArrayList<Integer> difference(ArrayList<Integer> diffList) {  // 1. Ищем модуль разницы чисел в списке 2. Находим сумму п.1 для каждого числа и добавляем в массив
 
-
-//        ArrayList<Integer> difference = new ArrayList<>();
-        ArrayList<Integer> diffSumArr = new ArrayList<>();
         int diffSum = 0;
+        ArrayList<Integer> diffSumArr = new ArrayList<>();
+
 
         for (int i = 0; i < diffList.size(); i++) {
 
             for (int j = 0; j < diffList.size(); j++) {
                 int differenceResult = Math.abs(diffList.get(j) - diffList.get(i));
-//                System.out.println("Первый элемент: " + diffList.get(j) + " Второй элемент: " + diffList.get(i));
-//                System.out.println("Результат вычитания: " + differenceResult);
                 if (differenceResult > 0) {
-//                    difference.add(differenceResult);
                     diffSum += differenceResult;
-
                 }
-
             }
             diffSumArr.add(diffSum);
             diffSum = 0;
-
         }
-
         return diffSumArr;
     }
 
-    /* Следующий этап разнести в массивы значения модулей разностей по каждому числу. затем посчитать сумму значений этих массивов. и сравнить
-    минимальная и будет медианой
-     */
-
-
-    public int findSmallest(ArrayList<Integer> outOfMedianaList) {
+    private int findSmallest(ArrayList<Integer> outOfMedianaList) {   // Находим минимальное значение из  списка
 
         ArrayList<Integer> resultList = difference(outOfMedianaList);
-
         int smallest = resultList.get(0);
         int smallest_index = 0;
 
@@ -49,15 +35,11 @@ public class Mediana {
                 smallest_index = i;
             }
         }
-
         return smallest_index;
     }
 
-
-    public int findMediana(ArrayList<Integer> startList) {
-
+    public int findMediana(ArrayList<Integer> startList) { // Находим медиану из списка
         int smallest_index = findSmallest(startList);
-
         return startList.get(smallest_index);
     }
 
