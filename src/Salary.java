0b36b9957397d52f7de6d0b557c5e817dc1da765
robotdorog <robotdorog@gmail.com>
2021-06-07 11:07:35 +0800

    Разарботка метода Salary шаг1

--- src/Salary.java
+++ src/Salary.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Salary {
@@ -9,7 +11,7 @@ public class Salary {
         Random random = new Random();
         return (random.nextInt(100 - 25) + 25) * 1000;
     }
-    public HashMap<String, Integer> fillIn(HashMap<String, Integer> salaryMap) {
+    public HashMap<String, Integer> fillIn() {
         salaryMap.put("Иван Петров", generateSalary());
         salaryMap.put("Петр Иванов", generateSalary());
         salaryMap.put("Василий Иванов", generateSalary());
@@ -21,8 +23,67 @@ public class Salary {
         salaryMap.put("Марина Вересова", generateSalary());
         salaryMap.put("Илья Игорев", generateSalary());
 
-
-
   return salaryMap;  }
 
+
+
+    private int findMin() {
+
+        ArrayList<Integer> values = new ArrayList<>(salaryMap.values());
+        int smallest = values.get(0);
+        int smallest_index = 0;
+
+        for (int i = 1; i < values.size(); i++) {
+            if (values.get(i) < smallest) {
+                smallest = values.get(i);
+                smallest_index = i;
+            }
+
+        }
+    return smallest;}
+
+    private int findMax() {
+
+        ArrayList<Integer> values = new ArrayList<>(salaryMap.values());
+        int biggest = values.get(0);
+        int biggest_index = 0;
+
+        for (int i = 1; i < values.size(); i++) {
+            if (values.get(i) < biggest) {
+                biggest = values.get(i);
+                biggest_index = i;
+            }
+
+        }
+        return biggest;}
+
+    public HashMap<Object, Object> checkMinMax (){
+        fillIn();
+        boolean test = false;
+        HashMap<Object, Object> hmMinMax = new HashMap<>();
+
+        for (Map.Entry entry : salaryMap.entrySet()) {
+
+            test = salaryMap.containsValue(findMin());
+            System.out.println("Значение тест: " + test);
+            if (test) {
+             hmMinMax.put(entry.getKey(), entry.getValue());
+            }
+//            test = false;
+//
+//            test =salaryMap.containsValue(findMax());
+//            if (test == true) {
+//              hmMinMax.put(entry.getKey(), entry.getValue());
+//            }
+
+        }
+    return hmMinMax;
+
+    }
+
+
+
 }
+
+
+
--- src/SimpleTask8.java
+++ src/SimpleTask8.java
@@ -3,5 +3,6 @@ public class SimpleTask8 {
     public static void main(String[] args) {
         Salary item = new Salary();
         System.out.println("Список сотрудников: " + item.fillIn());
+        System.out.println("Минимальная и Максимальная ЗП: " + item.checkMinMax());
     }
 }
