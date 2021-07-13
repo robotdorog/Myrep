76062e9cfd154d512800767f83c285c3dcc1dd1a
robotdorog <robotdorog@gmail.com>
2021-07-13 10:46:21 +0800

    Выполнены все условия задачи

--- src/Salary.java
+++ src/Salary.java
@@ -2,8 +2,10 @@ import java.util.*;
 
 public class Salary {
 
-    HashMap<String,  Integer> salaryMap = new HashMap<>();
-    HashMap<String, Integer> fillSalaryMap = fillIn(salaryMap);
+    HashMap<String, Integer> salaryMap = new HashMap<>();
+    HashMap<String, Integer> fillSalaryMap = fillIn();
+    ArrayList<Integer> arrInt = hashValues();
+    ArrayList<String> arrStr = hashKeys();
 
     private int generateSalary() {   //Метод для генерации зарплаты
         Random random = new Random();
@@ -11,7 +13,7 @@ public class Salary {
     }
 
 
-    private HashMap<String, Integer> fillIn(HashMap<String, Integer> hashMap) {  // Метод для заполнения мапы
+    private HashMap<String, Integer> fillIn() {  // Метод для заполнения мапы
         salaryMap.put("Иван Петров", generateSalary());
         salaryMap.put("Петр Иванов", generateSalary());
         salaryMap.put("Василий Иванов", generateSalary());
@@ -23,30 +25,27 @@ public class Salary {
         salaryMap.put("Марина Вересова", generateSalary());
         salaryMap.put("Илья Игорев", generateSalary());
 
-  return salaryMap;  }
+        return salaryMap;
+    }
 
 
-
-    private ArrayList<Integer> hashValues  (HashMap<String, Integer> hashArr) {
-        hashArr = fillSalaryMap;
+    private ArrayList<Integer> hashValues() {
+        HashMap<String, Integer> hashArr = fillSalaryMap;
 
         return new ArrayList<>(hashArr.values());
     }
 
 
-    private ArrayList<String> hashKeys (HashMap<String, Integer> hashArr) {
-        hashArr = fillSalaryMap;
+    private ArrayList<String> hashKeys() {
+        HashMap<String, Integer> hashArr = fillSalaryMap;
 
         return (ArrayList<String>) new ArrayList(hashArr.keySet());
     }
 
 
-
     public String findMin() {  //Метод поиска минимальной зарплаты
 
-        ArrayList<Integer> arrInt = hashValues(fillSalaryMap);
-        ArrayList<String> arrStr = hashKeys(fillSalaryMap);
-        String resultSmallest = "";
+        String resultSmallest;
         int smallest = arrInt.get(0);
         int smallest_index = 0;
 
@@ -58,14 +57,13 @@ public class Salary {
 
         }
         resultSmallest = "Самая маленькая ЗП: " + arrStr.get(smallest_index) + " " + arrInt.get(smallest_index);
-        return resultSmallest;}
+        return resultSmallest;
+    }
 
 
     public String findMax() { //Поиск максимальной зарплаты
 
-        ArrayList<Integer> arrInt = hashValues(fillSalaryMap);
-        ArrayList<String> arrStr = hashKeys(fillSalaryMap);
-        String resultBiggest = "";
+        String resultBiggest;
         int biggest = arrInt.get(0);
         int biggest_index = 0;
 
@@ -77,9 +75,60 @@ public class Salary {
 
         }
         resultBiggest = "Самая большая ЗП: " + arrStr.get(biggest_index) + " " + arrInt.get(biggest_index);
-        return resultBiggest;}
+        return resultBiggest;
+    }
 
 
+    private int averageValue() {  //Сделать private
+        int averageSalary = 0;
+        for (int i : arrInt) {
+            averageSalary = averageSalary + i;
+        }
+        averageSalary = averageSalary / arrInt.size();
+        return averageSalary;
+    }
+
+    public ArrayList<String> moreThanAverage() {
+        ArrayList<String> listMoreThanAverage = new ArrayList<>();
+        for (int i = 0; i < arrInt.size(); i++) {
+            if (arrInt.get(i) > averageValue()) {
+                listMoreThanAverage.add(arrStr.get(i));
+            }
+
+
+        }
+        return listMoreThanAverage;
+    }
+
+    public ArrayList<Integer> taxSalary() {
+        ArrayList<Integer> listTaxSalary = new ArrayList<>();
+        for (int i : arrInt) {
+            int taxSalary;
+            taxSalary = (i * 13) / 100;
+            listTaxSalary.add(taxSalary);
+
+        }
+        return listTaxSalary;
+    }
+
+    public ArrayList<Integer> salaryAfterTax() {
+        ArrayList<Integer> listSalaryAfterTax = new ArrayList<>();
+        for (int i = 0; i < arrInt.size(); i++) {
+            int salaryAfterTax;
+            salaryAfterTax = arrInt.get(i) - taxSalary().get(i);
+            listSalaryAfterTax.add(salaryAfterTax);
+        }
+        return listSalaryAfterTax;
+    }
+
+
+    public void taxResult() {
+        String strTaxResult;
+        for (int i = 0; i < arrInt.size(); i++) {
+            strTaxResult = ("ФИО: " + arrStr.get(i) + " На руки: " + salaryAfterTax().get(i) + " Налог: " + taxSalary().get(i));
+            System.out.println(strTaxResult);
+        }
+    }
 }
 
 
--- src/SimpleTask8.java
+++ src/SimpleTask8.java
@@ -2,9 +2,14 @@ public class SimpleTask8 {
 
     public static void main(String[] args) {
         Salary item = new Salary();
-//        System.out.println("Список сотрудников: " + item.fillIn());
         System.out.println("ЗП: " + item.fillSalaryMap);
+        System.out.println("");
         System.out.println(item.findMin());
+        System.out.println("");
         System.out.println(item.findMax());
+        System.out.println("");
+        System.out.println("Список сотрудников с ЗП выше средней: " + item.moreThanAverage());
+        System.out.println("");
+        item.taxResult();
     }
 }
