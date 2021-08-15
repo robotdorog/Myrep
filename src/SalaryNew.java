7f71742122286b010a0b413803dee89c091ab036
robotdorog <robotdorog@gmail.com>
2021-08-15 16:07:20 +0800

    Избавился от дублирующих списков. Исправлены замечания по комментариям

---
+++ src/SalaryNew.java
@@ -0,0 +1,116 @@
+import java.util.*;
+
+public class SalaryNew {
+    HashMap<String, Integer> salaryMap = new HashMap();
+
+    /**
+     * Конструктор. Заполнение HashMap
+     */
+    public SalaryNew() {
+
+        salaryMap.put("Иван Петров", generateSalary());
+        salaryMap.put("Петр Иванов", generateSalary());
+        salaryMap.put("Василий Иванов", generateSalary());
+        salaryMap.put("Мария Михайлова", generateSalary());
+        salaryMap.put("Елена Белова", generateSalary());
+        salaryMap.put("Юлия Ветрова", generateSalary());
+        salaryMap.put("Игорь Смирнов", generateSalary());
+        salaryMap.put("Екатерина Беглова", generateSalary());
+        salaryMap.put("Марина Вересова", generateSalary());
+        salaryMap.put("Илья Игорев", generateSalary());
+    }
+
+    /**
+     * Генерация ЗП
+     *
+     * @return
+     */
+    private int generateSalary() {
+        Random random = new Random();
+        return (random.nextInt(75) + 25) * 1000;
+    }
+
+    /**
+     * Поиск сотрудника с минимальной ЗП
+     *
+     * @return
+     */
+
+    public String salaryMinValue() {
+
+        int min = Integer.MAX_VALUE;
+        String maxSalaryEmployee = "";
+        String result;
+        for (int i : salaryMap.values()) {
+            if (i < min) {
+                min = i;
+            }
+        }
+
+        for (String s : salaryMap.keySet()) {
+            if (salaryMap.get(s) == min) {
+                maxSalaryEmployee = s;
+            }
+        }
+        return result = "Минимальная ЗП у сотрудника: " + maxSalaryEmployee + " равна: " + min;
+
+    }
+
+    /**
+     * Поиск сотрудника с максимальной ЗП
+     *
+     * @return
+     */
+    public String salaryMaxValue() {
+
+        int max = Integer.MIN_VALUE;
+        String maxSalaryEmployee = "";
+        String result;
+        for (int i : salaryMap.values()) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        for (String s : salaryMap.keySet()) {
+            if (salaryMap.get(s) == max) {
+                maxSalaryEmployee = s;
+            }
+        }
+        return result = "Максимальная ЗП у сотрудника: " + maxSalaryEmployee + " равна: " + max;
+    }
+
+    /**
+     * Поиск средней ЗП среди сотрудников
+     *
+     * @return
+     */
+    public String averageSalary() {
+
+        String result;
+        int amount = 0;
+        for (int i : salaryMap.values()) {
+            amount += i;
+        }
+        int average = amount / salaryMap.size();
+        return result = "Средняя ЗП среди сотрудников: " + average;
+    }
+
+    /**
+     * Список ЗП сотрудников после вычета налогов
+     *
+     * @return
+     */
+    public ArrayList<String> taxSlary() {
+        ArrayList<String> result = new ArrayList<>();
+        for (String s : salaryMap.keySet()) {
+            int taxSalary;
+            int afterTax;
+            taxSalary = (salaryMap.get(s) * 13) / 100;
+            afterTax = salaryMap.get(s) - taxSalary;
+            result.add("Сотрудник: " + s + "." + " ЗП после вычета налога: " + afterTax);
+
+        }
+        return result;
+    }
+
+}
--- src/SimpleTask8.java
+++ src/SimpleTask8.java
@@ -1,15 +1,13 @@
 public class SimpleTask8 {
 
     public static void main(String[] args) {
-        Salary item = new Salary();
-        System.out.println("ЗП: " + item.fillSalaryMap);
-        System.out.println("");
-        System.out.println(item.findMin());
-        System.out.println("");
-        System.out.println(item.findMax());
-        System.out.println("");
-        System.out.println("Список сотрудников с ЗП выше средней: " + item.moreThanAverage());
-        System.out.println("");
-        item.taxResult();
+        SalaryNew item = new SalaryNew();
+
+        System.out.println(item.salaryMap.toString());
+        System.out.println(item.salaryMinValue());
+        System.out.println(item.salaryMaxValue());
+        System.out.println(item.averageSalary());
+        System.out.println(item.taxSlary());
+
     }
 }
